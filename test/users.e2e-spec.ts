6f42a642aa0d92dd7389e46360f64e356b4a51c7
Samandar Sunnatov <sunnatovsamandar77@gmail.com>
2024-03-31 13:20:36 +0500

    e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from "../src/app.module";
 
 describe("User (e2e)", () => {
   let app: INestApplication;
-  let token: string;//asdfasdf
+  let token: string;//asdfasdfasdfadsf
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
